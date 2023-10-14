9d91dd79e326e2b56cfa09995f414eef2b7061ca
Arogya Kharel <kharelarogya@hotmail.com>
2023-10-15 00:30:16 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by arogya.Gurkha on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
